04408d27ed30b7493d45d7df1e9a97a1a574f222
Raihan567 <aburaihansarker567@gmail.com>
2022-09-28 15:12:44 +0600

    declared constant

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h2>Counter Application</h2>
+      <h3>Counter: 0</h3>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/Services/Constant/counterConstant.js
@@ -0,0 +1,7 @@
+// constant
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+export { INCREMENT, DECREMENT, RESET };
